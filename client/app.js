866049fd9b391ad0b30f056d1df88fcb8e67c2de
Spencer Coltharp <spencer.colth@gmail.com>
2022-12-15 13:19:05 -0600

    Test

--- client/app.js
+++ client/app.js
@@ -10,6 +10,6 @@ fetch("/api/inventory").then(res => res.json()).then((data) => {
 
 
 
-fetch("/api/inventory/:id").then(res => res.json()).then((data) => {
+fetch("/api/inventory/:").then(res => res.json()).then((data) => {
     console.log(data);
 })
