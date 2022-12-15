42a4bb299d84603cf3f18264cfa2644b2ff0c0dc
Spencer Coltharp <115845587+spencerColth@users.noreply.github.com>
2022-12-15 12:20:47 -0600

    Add files via upload

--- client/app.js
+++ client/app.js
@@ -1,1 +1,15 @@
 
+
+
+
+
+
+fetch("/api/inventory").then(res => res.json()).then((data) => {
+    console.log(data);
+})
+
+
+
+fetch("/api/inventory/:id").then(res => res.json()).then((data) => {
+    console.log(data);
+})
